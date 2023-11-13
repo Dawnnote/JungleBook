9fd6d1088fdaa3a347d6bbf539d75a6a1a2b9353
Dawnnote <lshoon7454@gmail.com>
2023-11-13 16:07:20 +0900

    11/13/2023

---
+++ src/main/java/com/example/junglebook/JunglebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.junglebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunglebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunglebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/junglebook/JunglebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junglebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunglebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
