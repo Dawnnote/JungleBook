593b0d67452049dcdef4f1e5759541984c692147
KGY <boy25860@github.com>
2023-11-28 16:15:55 +0900

    판매 추가

---
+++ src/main/java/com/example/junglebook/controller/SellBookMainController.java
@@ -0,0 +1,12 @@
+package com.example.junglebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SellBookMainController {
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/sell_post/create";
+    }
+}
